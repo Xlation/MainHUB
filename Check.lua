fb146187d37a3e855432e062a2f905e96d27f336
Xlation <121344251+Xlation@users.noreply.github.com>
2023-01-28 14:06:15 +0700

    Create Check.lua

---
+++ Check.lua
@@ -0,0 +1,51 @@
+if not game:IsLoaded() then 
+	repeat game.Loaded:Wait()
+	until game:IsLoaded() 
+end
+local hwid = false
+local hwidplr = game:GetService("RbxAnalyticsService"):GetClientId()
+local allchecked = false
+
+local basickey = false
+local specialkey = false
+
+--KEY
+
+if _G.Key == "ADMINKEY14522SPC" then
+    hwid = "39D2ABA1-2D5B-4419-8F8B-2146B43AB52C"
+    specialkey = true
+end
+
+if _G.Key == "ADMINKEY14522BASIC" then
+    hwid = "39D2ABA1-2D5B-4419-8F8B-2146B43AB52C"
+    basickey = true
+end
+
+
+
+
+--
+
+if basickey == true then
+    if hwidplr == hwid and basickey == true then
+        allchecked = true
+        wait(0.5)
+       loadstring(game:HttpGet('https://xlationhub.000webhostapp.com/Script.lua'))() 
+    else
+        game.Players.LocalPlayer:Kick("Invalid Hwid!")
+    end
+end
+
+if specialkey == true then
+    if hwidplr == hwid and specialkey == true then
+        allchecked = true
+        wait(0.5)
+       loadstring(game:HttpGet('https://xlationhub.000webhostapp.com/ScriptSpecial.lua'))()
+    else
+        game.Players.LocalPlayer:Kick("Invalid Hwid!")
+    end
+end
+
+if basickey == false and specialkey == false then
+    game.Players.LocalPlayer:Kick("Invalid Key!") 
+end
