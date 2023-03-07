8f2efb7e41633b6eb640dad26250c0b64c70b7ed
Xlation <121344251+Xlation@users.noreply.github.com>
2023-03-07 14:11:55 +0700

    Update Check.lua

--- Check.lua
+++ Check.lua
@@ -2,34 +2,34 @@ if not game:IsLoaded() then
 	repeat game.Loaded:Wait()
 	until game:IsLoaded() 
 end
-local hwid = false
+_G.hwid = false
 local hwidplr = game:GetService("RbxAnalyticsService"):GetClientId()
-local allchecked = false
+_G.allchecked = false
 
-local basickey = false
-local specialkey = false
+_G.basickey = false
+_G.specialkey = false
 
 --KEY
 
 if _G.Key == "ADMINKEY14522SPC" then
-    hwid = "39D2ABA1-2D5B-4419-8F8B-2146B43AB52C"
-    specialkey = true
+    _G.hwid = "39D2ABA1-2D5B-4419-8F8B-2146B43AB52C"
+    _G.specialkey = true
 end
 
 if _G.Key == "ADMINKEY14522BASIC" then
-    hwid = "39D2ABA1-2D5B-4419-8F8B-2146B43AB52C"
-    basickey = true
+    _G.hwid = "39D2ABA1-2D5B-4419-8F8B-2146B43AB52C"
+    _G.basickey = true
 end
 
 if _G.Key == "NAHEEKEY"
-hwid = "testkey"
-basickey = true
+_G.hwid = "testkey"
+_G.basickey = true
 hwidplr = "testkey"
 end
 
 if _G.Key == "NongSomNaHeeMakMak"
-hwid = "testkey"
-specialkey = true
+_G.hwid = "testkey"
+_G.specialkey = true
 hwidplr = "testkey"
 end
 
@@ -37,26 +37,26 @@ end
 
 --
 while wait() do
-	if allchecked == false then
-		if basickey == true then
-    if hwidplr == hwid and basickey == true then
-        allchecked = true
+	if _G.allchecked == false then
+		if _G.basickey == true then
+    if hwidplr == _G.hwid and _G.basickey == true then
+        _G.allchecked = true
        loadstring(game:HttpGet('https://raw.githubusercontent.com/Xlation/MainHUB/main/BasicScript.lua'))() 
     else
         game.Players.LocalPlayer:Kick("Invalid Hwid!")
     end
 end
 
-if specialkey == true then
-    if hwidplr == hwid and specialkey == true then
-        allchecked = true
+if _G.specialkey == true then
+    if hwidplr == _G.hwid and _G.specialkey == true then
+        _G.allchecked = true
        loadstring(game:HttpGet('https://raw.githubusercontent.com/Xlation/MainHUB/main/SpecialScript.lua'))()
     else
         game.Players.LocalPlayer:Kick("Invalid Hwid!")
     end
 end
 
-if basickey == false and specialkey == false then
+if _G.basickey == false and _G.specialkey == false then
     game.Players.LocalPlayer:Kick("Invalid Key!") 
 end
 	end
