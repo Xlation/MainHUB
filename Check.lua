a2ff85e5de1bd383e04625a0ea167089b51553a0
Xlation <121344251+Xlation@users.noreply.github.com>
2023-07-24 11:34:06 +0700

    Update Check.lua

--- Check.lua
+++ Check.lua
@@ -1,69 +1,47 @@
-local plyhwid = game:GetService("RbxAnalyticsService"):GetClientId()
-local mainhwid = "nill"
-local specialkey = false
-local basickey = false
-local havekey = false
 
+local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
+local keylist = {
+["XLT-KEY14522"] = hwid,
+["S9KK-0EX1"] = hwid,
+["428P-F2R6"] = hwid,
+["03W6-4ZGI"] = hwid,
+["8PMJ-AUMY"] = hwid,
+["HRBK-X7WC"] = hwid,
+["QTZ0-4025"] = hwid,
+["ZWGS-F24D"] = hwid,
+["HZ6D-4LUS"] = hwid,
+["8Y41-5549"] = hwid,
+["65EE-IGT9"] = hwid,
+["PM84-8LYO"] = hwid,
+["P69C-KHLN"] = hwid,
+["CANZ-RMKM"] = hwid,
+["WMDW-P7IW"] = hwid,
+["GDYS-NN2J"] = hwid,
+["LL52-735I"] = hwid,
+["PX4A-GUZS"] = hwid,
+["ABFW-DQE7"] = hwid,
+["MVAX-7CCN"] = hwid,
+["V6R4-E1E4"] = hwid,
+["K098-ECSO"] = hwid,
+["H4LI-1FON"] = hwid,
+["GR3F-ISQL"] = hwid,
+["0Z9N-S6SA"] = hwid,
+["7AHS-3S4E"] = hwid,
+["YT6G-CINH"] = hwid,
+["V5KU-R66T"] = hwid,
+["A9Q9-26D2"] = hwid,
+["N4P3-NVQB"] = hwid,
+["U7RV-UOBE"] = hwid,
+}
 
---KEY
-if _G.Key == "ADMIN-XLATION-SPC" then
-	mainhwid = "plyhwid"
-	havekey = true
-	specialkey = true
-end
-
-if _G.Key == "9CGmq62LkJ" then
-	mainhwid = "plyhwid"
-	havekey = true
-	specialkey = true
-end
-
-if _G.Key == "WdFDjHiWhY" then
-	mainhwid = "plyhwid"
-	havekey = true
-	specialkey = true
-end
-
-if _G.Key == "N5vpzpbk7Q" then
-	mainhwid = "plyhwid"
-	havekey = true
-	specialkey = true
-end
-
-if _G.Key == "3aHzTxr4Vi" then
-	mainhwid = "plyhwid"
-	havekey = true
-	specialkey = true
-end
-
-if _G.Key == "VTDCGAoFc5" then
-	mainhwid = "plyhwid"
-	havekey = true
-	specialkey = true
-end
-
-if _G.Key == "9CGmq62LkJ" then
-	mainhwid = "plyhwid"
-	havekey = true
-	specialkey = true
-end
-
---
-
-if havekey == true then
-	if specialkey == true then
-		if plyhwid == mainhwid then
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/Xlation/MainHUB/main/SpecialScript.lua"))()
-		else
-			game.Players.LocalPlayer:Kick("XlationHub - Invaild Hwid")
-		end
-	elseif basickey == true then
-		if plyhwid == mainhwid then
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/Xlation/MainHUB/main/BasicScript.lua"))()
-		else
-			game.Players.LocalPlayer:Kick("XlationHub - Invaild Hwid")
-		end
-	end
+if keylist[_G.Key] then
+    if hwid == keylist[_G.Key] then
+        local whitelistGui = Instance.new("ScreenGui")
+        whitelistGui.Name = "WFruits"
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Xlation/MainHUB/main/SpecialScript.lua"))();
+    else
+        game.Players.LocalPlayer:kick("Invaild Hwid")
+    end
 else
-	game.Players.LocalPlayer:Kick("XlationHub - Invaild Key")
+    game.Players.LocalPlayer:kick("Invaild Key")
 end
